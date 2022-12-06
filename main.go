08d5659df2fa0bb320b18653f1c27757bacdaecd
yinshuwei <yinshuwei@foxmail.com>
2022-12-06 12:08:07 +0800

    all

--- main.go
+++ main.go
@@ -18,40 +18,6 @@ type User struct {
 	CreateTime *time.Time
 }
 
-// Logger 适配zap logger
-type Logger struct {
-	zapLogger *zap.Logger
-}
-
-func (l *Logger) fields(data map[string]string) []zap.Field {
-	var fields []zap.Field
-	for key, val := range data {
-		fields = append(fields, zap.String(key, val))
-	}
-	return fields
-}
-
-func (l *Logger) Error(msg string, data map[string]string) {
-	if l == nil || l.zapLogger == nil {
-		return
-	}
-	l.zapLogger.Error(msg, l.fields(data)...)
-}
-
-func (l *Logger) Info(msg string, data map[string]string) {
-	if l == nil || l.zapLogger == nil {
-		return
-	}
-	l.zapLogger.Info(msg, l.fields(data)...)
-}
-
-func (l *Logger) Warn(msg string, data map[string]string) {
-	if l == nil || l.zapLogger == nil {
-		return
-	}
-	l.zapLogger.Warn(msg, l.fields(data)...)
-}
-
 func stringPoint(t string) *string {
 	return &t
 }
