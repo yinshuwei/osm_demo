b9dc684b4fc5ba6fc1d15326f3948f5eff071071
yinshuwei <yinshuwei@foxmail.com>
2022-12-06 12:06:40 +0800

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module osm_demo
+
+go 1.16
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/yinshuwei/osm/v2 v2.0.3
+	go.uber.org/zap v1.24.0
+)
---
+++ main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/yinshuwei/osm/v2"
+	"go.uber.org/zap"
+)
+
+// User 用户Model
+type User struct {
+	ID         *int64
+	Email      *string
+	Nickname   *string
+	CreateTime *time.Time
+}
+
+// Logger 适配zap logger
+type Logger struct {
+	zapLogger *zap.Logger
+}
+
+func (l *Logger) fields(data map[string]string) []zap.Field {
+	var fields []zap.Field
+	for key, val := range data {
+		fields = append(fields, zap.String(key, val))
+	}
+	return fields
+}
+
+func (l *Logger) Error(msg string, data map[string]string) {
+	if l == nil || l.zapLogger == nil {
+		return
+	}
+	l.zapLogger.Error(msg, l.fields(data)...)
+}
+
+func (l *Logger) Info(msg string, data map[string]string) {
+	if l == nil || l.zapLogger == nil {
+		return
+	}
+	l.zapLogger.Info(msg, l.fields(data)...)
+}
+
+func (l *Logger) Warn(msg string, data map[string]string) {
+	if l == nil || l.zapLogger == nil {
+		return
+	}
+	l.zapLogger.Warn(msg, l.fields(data)...)
+}
+
+func stringPoint(t string) *string {
+	return &t
+}
+
+func timePoint(t time.Time) *time.Time {
+	return &t
+}
+
+func main() {
+	o, err := osm.New("mysql", "root:123456@/test?charset=utf8", osm.Options{})
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	logger, _ := zap.NewDevelopment()
+
+	osm.ConfLogger(&Logger{logger}, &Logger{logger}, true, 0)
+
+	{ //添加
+		user := User{
+			Email:      stringPoint("test@foxmail.com"),
+			Nickname:   nil,
+			CreateTime: timePoint(time.Now()),
+		}
+		id, count, err := o.Insert("INSERT INTO user (email,nickname,create_time) VALUES (#{Email},#{Nickname},#{CreateTime});", user)
+		if err != nil {
+			logger.Error("insert error", zap.Error(err))
+		}
+		logger.Info("test insert", zap.Int64("id", id), zap.Int64("count", count))
+	}
+
+	{ //查询
+		user := User{
+			Email: stringPoint("test@foxmail.com"),
+		}
+		var results []User
+		count, err := o.SelectStructs("SELECT id,email,nickname,create_time FROM user WHERE email=#{Email};", user)(&results)
+		if err != nil {
+			logger.Error("test select", zap.Error(err))
+		}
+		resultBytes, _ := json.Marshal(results)
+		logger.Info("test select", zap.Int64("count", count), zap.ByteString("result", resultBytes))
+	}
+
+	{ // 更新
+		user := User{
+			Email:    stringPoint("test@foxmail.com"),
+			Nickname: stringPoint("hello"),
+		}
+		count, err := o.Update("UPDATE user SET nickname=#{Nickname} WHERE email=#{Email}", user)
+		if err != nil {
+			logger.Error("update error", zap.Error(err))
+		}
+		logger.Info("test update", zap.Int64("count", count))
+	}
+
+	{ //查询
+		user := User{
+			Email: stringPoint("test@foxmail.com"),
+		}
+		var results []User
+		count, err := o.SelectStructs("SELECT id,email,nickname,create_time FROM user WHERE email=#{Email};", user)(&results)
+		if err != nil {
+			logger.Error("test select", zap.Error(err))
+		}
+		resultBytes, _ := json.Marshal(results)
+		logger.Info("test select", zap.Int64("count", count), zap.ByteString("result", resultBytes))
+	}
+
+	{ //删除
+		user := User{
+			Email: stringPoint("test@foxmail.com"),
+		}
+		count, err := o.Delete("DELETE FROM user WHERE email=#{Email}", user)
+		if err != nil {
+			logger.Error("test delete", zap.Error(err))
+		}
+		logger.Info("test delete", zap.Int64("count", count))
+	}
+
+	{ //关闭
+		err = o.Close()
+		if err != nil {
+			logger.Error("close", zap.Error(err))
+		}
+	}
+}
